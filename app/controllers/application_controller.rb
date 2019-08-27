e00e955dfeb3c1e1322f701975a91e78f68aa5cc
Anais Ahmed <anaismahmed@uchicago.edu>
2019-08-27 14:20:40 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -5,5 +6,52 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+
+  get '/' do
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @name = params[:name]
+    @ingredients = params[:ingredients]
+    @cook_time = params[:cook_time]
+    recipe = Recipe.create(name: @name, ingredients: @ingredients, cook_time: @cook_time)
+    redirect to "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+
 
 end
